55fc7dedb393d74b4340d17db65cafabda3c541c
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-01-08 12:03:55 -0800

    Added counter

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView welcome_text, counter_text;
     Button btn;
-
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         welcome_text = findViewById(R.id.welcome_text);
 
         //Adding functionality
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter_text.setText(""+increaseCounter());
 
+            }
+        });
+    }
+
+    public int increaseCounter(){
+        return ++counter;
     }
 }
