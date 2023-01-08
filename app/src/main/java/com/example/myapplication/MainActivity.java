ae3274c7ed6365daa01ba087ddb6821aefa29e63
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-01-08 12:00:07 -0800

    Added views and functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView welcome_text, counter_text;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        counter_text = findViewById(R.id.counter_text);
+        welcome_text = findViewById(R.id.welcome_text);
+
+        //Adding functionality
+
     }
 }
